ba5d0d075efff8d15eceac02c90a90291fd8c0c2
scalenex <scalenextechnologies@gmail.com>
2025-03-21 17:22:05 +0530

    ""

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -10,7 +10,7 @@ export const clerkWebhooks = async (req, res) => {
 
       await whook.verify(JSON.stringify(req.body), {
          "svix-id": req.headers["svix-id"],
-         'svix-timestamp': req.headers["svix-timestamp"],
+         "svix-timestamp": req.headers["svix-timestamp"],
          "svix-signature": req.headers["svix-signature"],
       })
 
