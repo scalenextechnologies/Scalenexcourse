8cae597620407e9f674cf50f3b1a377fbfb3ddfd
scalenex <scalenextechnologies@gmail.com>
2025-04-11 11:09:37 +0530

    most changes

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express'
-import { updateRoleToEducator } from '../controllers/educatorController.js'
+import { addCourse, updateRoleToEducator } from '../controllers/educatorController.js'
+import upload from '../config/multer.js'
+import { protectEducator } from '../middleware/authMiddleware.js'
 
 
 const educatorRouter=express.Router()
@@ -8,5 +10,7 @@ const educatorRouter=express.Router()
 //Add Educator Role
 
 educatorRouter.get('/update-role',updateRoleToEducator)
+
+educatorRouter.post('/add-course',upload.single('image'),protectEducator,addCourse)
 
 export default educatorRouter;
--- server/server.js
+++ server/server.js
@@ -6,16 +6,19 @@ import connectDB from './config/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './config/cloudinary.js'
 
 //Intialise Express
 const app=express()
 
 //connect to database
 await connectDB()
+await connectCloudinary()
 
 //Middlewear
 app.use(cors())
 app.use(clerkMiddleware())
+
 
 //Route
 
