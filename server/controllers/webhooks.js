56f36d3850ff86f69be06b7f9e54a489aa255b87
scalenex <scalenextechnologies@gmail.com>
2025-04-25 11:23:09 +0530

    change web

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -107,24 +107,23 @@ export const stripeWebhooks = async (request, response) => {
         console.log('Purchase updated to completed')
 
         // Enroll the user in the course
-        const userData = await User.findOne(purchaseData.userId)
+        const userData = await User.findById(purchaseData.userId)
         const courseData = await Course.findById(purchaseData.courseId.toString())
 
         if (!userData || !courseData) {
           console.error('User or Course not found')
           return response.status(404).send('User or Course not found')
         }
-
-        if (!courseData.enrolledStudents.includes(userData._id)) {
+        if (!courseData.enrolledStudents.some(id => id.equals(userData._id))) {
           courseData.enrolledStudents.push(userData._id)
           await courseData.save()
         }
-
-        if (!userData.enrolledCourses.includes(courseData._id)) {
+        
+        if (!userData.enrolledCourses.some(id => id.equals(courseData._id))) {
           userData.enrolledCourses.push(courseData._id)
           await userData.save()
         }
-
+        
         console.log(`User ${userData._id} enrolled in course ${courseData._id}`)
 
         break
